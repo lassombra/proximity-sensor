51df11978efeb415c57c2a38f005a8c11399b718
lassombra <briatt22@gmail.com>
2024-12-21 01:36:35 -0600

    Initial item build

---
+++ unity-project/proximity-sensor/Assets/Editor/Export.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using UnityEditor;
+
+public class CreateAssetBundles
+{
+    [MenuItem("Assets/Build AssetBundles")]
+    static void BuildAllAssetBundles()
+    {
+        string assetBundleDirectory = "Bundles";
+        if (!Directory.Exists(assetBundleDirectory))
+        {
+            Directory.CreateDirectory(assetBundleDirectory);
+        }
+        BuildPipeline.BuildAssetBundles(assetBundleDirectory, 
+                                        BuildAssetBundleOptions.None, 
+                                        BuildTarget.StandaloneWindows);
+        File.Copy($"{assetBundleDirectory}/item", "../../item/item", true);
+    }
+}
